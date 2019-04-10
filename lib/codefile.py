7ceff4c6864a95bcd159c09c21b246603c7e4d6c
WustmannMatthias <m.wustmann@redspher.com>
2019-04-10 13:54:47 +0200

    Parser complete

--- lib/codefile.py
+++ lib/codefile.py
@@ -4,10 +4,11 @@
 
 from lib.settings import *
 
+import lib.repository as repository
+
 import os
 import re
 import json
-import ntpath
 
 
 class CodeFile:
@@ -17,9 +18,11 @@ class CodeFile:
         self.size = self.pick_up_size()
         self.loc = self.pick_up_loc()
 
+        self.current_namespace = '\\'
         self.variables = dict()
         self.files_included = list()
-        self.classes_used = list()
+        self.classes = list()
+        self.uses = list()
 
     def pick_up_loc(self):
         """Returns number of lines of code into file"""
@@ -35,7 +38,8 @@ class CodeFile:
 
     def parse(self):
         """
-            Parse entire file : store files included and classes used
+            Parse entire file : store files included, classes, variables,
+            current namespaces and uses clauses
         """
         in_comment = False
         with open(self.path, 'r') as fh:
@@ -52,16 +56,39 @@ class CodeFile:
                 if '*/' not in line: continue
                 in_comment = False
 
+            self.parse_namespace(line, line_counter)
             self.parse_variables(line, line_counter)
             self.parse_includes(line, line_counter)
-            #self.parse_classes(line, line_counter)
-            #self.parse_uses(line, line_counter)
+            self.parse_classes(line, line_counter)
+            self.parse_uses(line, line_counter)
 
 
+
+
+    #####################################################################
+    ############################# VARIABLES #############################
+    #####################################################################
+
+    def parse_namespace(self, line, line_count):
+        """Parse namespaces and store the current namespace in self.current_namespace """
+        regex = r"namespace\s+(?P<name>[a-zA-Z0-9_\\]+)\s*(\{|\;)"
+        match = re.match(regex, line)
+        if not match:
+            return
+        namespace = match.group('name')
+        if namespace.startswith('\\') and len(namespace) > 1:
+            namespace = namespace[1:]
+        self.current_namespace = namespace
+
+
+
+    #####################################################################
+    ############################# VARIABLES #############################
+    #####################################################################
 
     def parse_variables(self, line, line_count):
         """Parse every variable found in the file and store them in self.variables"""
-        regex = "^(?P<variable>\$[a-zA-Z0-9_]+)\s*=\s*[\"\']?(?P<value>[^\"\'\(\)]+)[\"\']?;"
+        regex = r"^(?P<variable>\$[a-zA-Z0-9_]+)\s*=\s*[\"\']?(?P<value>[^\"\'\(\)]+)[\"\']?;"
         matches = re.match(regex, line)
         if not matches:
             return
@@ -69,15 +96,26 @@ class CodeFile:
         value = matches.group('value')
         self.variables[variable] = value
 
+    class VariableNotFoundException(Exception):
+        def __init__(self, file, line_number, line, variable):
+            self.msg = f'File {file} line {line_number} : tried to solve variable "{variable}" in line {line}, but declaration was either not found or not understood.'
+            super(VariableNotFoundException, self).__init__(self.msg)
+
+        def __str__(self):
+            return self.msg
 
 
+
+    #####################################################################
+    ######################## INCLUDES / REQUIRE #########################
+    #####################################################################
 
     def parse_includes(self, line, line_counter):
         """
             Check for presence of "include" or "require" clauses in the given line,
             and store their parsed and rebuilt argument in self.files_included
         """
-        regex = "(?P<clause>((require)|(include)))(?P<once>(_once)?)\s+(?P<argument>[-_ A-Za-z0-9\$\.\"'\/\s\[\]]+);"
+        regex = r"(?P<clause>((require)|(include)))(?P<once>(_once)?)\s+(?P<argument>[-_ A-Za-z0-9\$\.\"'\/\s\[\]]+);"
         matches = re.match(regex, line)
         if not matches:
             return
@@ -87,7 +125,7 @@ class CodeFile:
         else: once = False
 
         path = self.parse_include_argument(argument, line_counter)
-        include = Include(path, clause, once, self.path, line_counter)
+        include = Include(path, clause, once, repository.Repository.path_from_repo(self.path, self.repo_name), line_counter)
         self.files_included.append(include)
 
 
@@ -96,18 +134,19 @@ class CodeFile:
         argument = self.replace_rel_path(argument, line_counter)
         argument = self.replace_magic_constant(argument, line_counter)
         argument = self.replace_variable(argument, line_counter)
-
-        return argument
+        argument = self.remove_unnecessary(argument, line_counter)
+        path = self.build_include_path(argument, line_counter)
+        return path
 
 
     def replace_rel_path(self, line, line_counter):
         """If $_SERVER['REL_PATH'] found in line, replace it with its value"""
-        regex = "(?P<relpath>\$_SERVER\s*\[\s*[\\]?[\"\']REL_PATH[\\]?[\"\']\s*\])"
+        regex = r"(?P<relpath>\$_SERVER\s*\[\s*[\\]?[\"\']REL_PATH[\\]?[\"\']\s*\])"
         matches = re.match(regex, line)
         if not matches:
             return line
         before_repo_name = self.path.split(self.repo_name)[0]
-        return line.replace(matches.group('relpath'), before_repo_name)
+        return line.replace(matches.group('relpath'), '"' + before_repo_name + '"')
 
 
     def replace_magic_constant(self, line, line_counter):
@@ -116,31 +155,118 @@ class CodeFile:
         matches = re.match(regex, line)
         if not matches:
             return line
-        current_dir = ntpath.dirname(self.path)
-        return line.replace(matches.group('magicconstant'), current_dir)
+        current_dir = os.path.dirname(self.path)
+        return line.replace(matches.group('magicconstant'), '"' + current_dir + '"')
 
 
     def replace_variable(self, line, line_counter):
         """If variables in line, try to replace them with their value"""
-        regex = "(\$[a-zA-Z0-9_]+)+"
+        regex = r"(\$[a-zA-Z0-9_]+)+"
         for variable in re.findall(regex, line):
             if variable in self.variables.keys():
-                line = line.replace(variable, self.variables[variable])
+                line = line.replace(variable, f'"{self.variables[variable]}"')
             else:
-                raise VariableNotFoundException(Repository.path_from_repo(self.path, self.repo_name), line, line_counter, variable)
+                raise VariableNotFoundException(repository.Repository.path_from_repo(self.path, self.repo_name), line_counter, line, variable)
         return line
 
 
+    def remove_unnecessary(self, line, line_counter):
+        """
+            Removes everything that doesn't belongs to the path, like the quotes/double-quotes
+            or the concatenation operators
+        """
+        inner = False
+        path = str()
+        for car in line:
+            if car in ['"', '"']:
+                inner = not inner
+                continue
+            if inner: path += car
+        return path
+
+
+    def build_include_path(self, path, line_counter):
+        """
+            Tries to build the absolute path by replacing .. and . by the right directories,
+            and raises exc
+        """
+        if os.path.isabs(path):
+            if not os.path.isfile(path):
+                raise DependencyNotFoundException(repository.Repository.path_from_repo(self.path, self.repo_name), line_counter, path)
+        else:
+            current_dir = os.path.dirname(self.path)
+            max_depth_allowed = 20
+            while not os.path.isfile(os.path.join(current_dir, path)):
+                if max_depth_allowed <= 0:
+                    raise AbsolutePathReconstructionException(repository.Repository.path_from_repo(self.path, self.repo_name), line_counter, path)
+                current_dir = self.remove_last_dir_in_path(current_dir)
+                max_depth_allowed -= 1
+            path = os.path.join(current_dir, path)
+        return os.path.normpath(path)
+
+
+    def remove_last_dir_in_path(self, path):
+        """Ex : sth/other/dir -> sth/other"""
+        return os.path.sep.join(path.split(os.path.sep)[:-1])
+
+
+    class AbsolutePathReconstructionException(Exception):
+        def __init__(self, file, line_number, path):
+            self.msg = f'File {file} line {line_number} : tried to rebuild path "{path}", but the file probably doesn\'t exist."'
+            super(AbsolutePathReconstructionException, self).__init__(self.msg)
+
+        def __str__(self):
+            return self.msg
+
+    class DependencyNotFoundException(Exception):
+        def __init__(self, file, line_number, path):
+            self.msg = f'File {file} line {line_number} : included file "{path}" doesn\'t exists."'
+            super(DependencyNotFoundException, self).__init__(self.msg)
+
+        def __str__(self):
+            return self.msg
 
 
 
-class VariableNotFoundException(Exception):
-    def __init__(self, file, line, line_number, variable):
-        self.msg = f'File {file} line {line_number} : tried to solve variable "{variable}" in line {line}, but declaration was either not found or not understood.'
-        super(VariableNotFoundException, self).__init__(self.msg)
+    #####################################################################
+    ############################# CLASSES ###############################
+    #####################################################################
 
-    def __str__(self):
-        return self.msg
+    def parse_classes(self, line, line_counter):
+        """Parse classes and store Class objects in self.classes"""
+        regex = r"class\s(?P<name>[a-zA-Z0-9_]+)"
+        match = re.match(regex, line)
+        if not match:
+            return
+        classname = match.group('name')
+
+        if classname.startswith('\\'):
+            namespace, classname = '\\', classname[1:]
+        else:
+            namespace, classname = self.current_namespace, classname
+        class_object = Class(namespace, classname, repository.Repository.path_from_repo(self.path, self.repo_name), line_counter)
+        self.classes.append(class_object)
+
+
+
+    #####################################################################
+    ############################### USES ################################
+    #####################################################################
+
+    def parse_uses(self, line, line_counter):
+        """Parse uses clauses and store Class objects in self.uses"""
+        regex = r"use\s+(?P<argument>[a-zA-Z0-9_\\]+)\s*"
+        match = re.match(regex, line)
+        if not match:
+            return
+        argument = match.group('argument')
+
+        splited_argument = argument.split('\\')
+        namespace = '\\'.join(splited_argument[:-1])
+        classname = splited_argument[-1]
+
+        class_object = Class(namespace, classname, repository.Repository.path_from_repo(self.path, self.repo_name), line_counter)
+        self.uses.append(class_object)
 
 
 
@@ -168,18 +294,26 @@ class Include:
 
 
     def get_relationship(self):
-        return Dependency.RELATIONSHIPS[self.type]
+        return Include.RELATIONSHIPS[self.type]
 
 
 
-class WrongIncludeType(Exception):
-    def __init__(self, parent, inclusion_line, type):
-        self.msg = f"Incorrect dependency relation found in {parent} on line {inclusion_line} : {type}"
-        super(WrongIncludeType, self).__init__(self.msg)
+    class WrongIncludeType(Exception):
+        def __init__(self, parent, inclusion_line, type):
+            self.msg = f"Incorrect dependency relation found in {parent} on line {inclusion_line} : {type}"
+            super(WrongIncludeType, self).__init__(self.msg)
 
-    def __str__(self):
-        return self.msg
+        def __str__(self):
+            return self.msg
 
+
+class Class:
+    """Represents a class found in a php file"""
+    def __init__(self, namespace, classname, parent, inclusion_line):
+        self.namespace = namespace
+        self.classname = classname
+        self.parent = parent
+        self.inclusion_line = inclusion_line
 
 
 
--- test.py
+++ test.py
@@ -9,17 +9,18 @@ from lib.codefile import CodeFile, Include
 
 
 def main():
-    #repo_path = "/var/www/html/type_test_repo"
-    repo_path = "/var/www/html/X"
-    repo = Repository(path=repo_path, directories_to_ignore="type_test_repo/Products", files_to_ignore="type_test_repo/Vehicules/vehicule_parts/Wheels.php")
+    repo_path = "/var/www/html/type_test_repo"
+    #repo_path = "/var/www/html/X"
+    repo = Repository(path=repo_path, directories_to_ignore=[".git", "Products"], files_to_ignore=["Vehicules/vehicule_parts/Wheels.php"])
     repo.build_filepath_list()
 
     for file in repo.filepath_list:
         codefile = CodeFile(file, repo.repo_name)
         print(codefile.path)
         codefile.parse()
-        for include in codefile.files_included:
-            print(include.path)
+        print(codefile.current_namespace)
+        for c in codefile.uses:
+            print(c.namespace, c.classname)
         print()
 
 
